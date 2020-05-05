fc94473b34649bb59fda898269ab211035c49308
temirlan <rakhimgaliev56@gmail.com>
2020-05-05 03:54:25 +0300

    bluetooth with gyro and accels works

--- bluetooth.cpp
+++ bluetooth.cpp
@@ -1,65 +1,54 @@
-/*
-    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleWrite.cpp
-    Ported to Arduino ESP32 by Evandro Copercini
-*/
+#include "BluetoothSerial.h"
+#include <TroykaIMU.h>
 
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
+BluetoothSerial SerialBT;
+String MACadd = "AA:BB:CC:11:22:33";
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
+Gyroscope gyro;
+Accelerometer accel;
+char buffer[256];
+char* measurement(){
+    sprintf(buffer,
+    "%f;%f;%f;%f;%f;%f;%d;",
+    accel.readAX(),
+    accel.readAY(),
+    accel.readAZ(),
+    gyro.readDegPerSecX(),
+    gyro.readDegPerSecY(),
+    gyro.readDegPerSecZ(),
+    millis());
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+    return buffer;
+}
 
-
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string value = pCharacteristic->getValue();
-
-      if (value.length() > 0) {
-        Serial.println("*********");
-        Serial.print("New value: ");
-        for (int i = 0; i < value.length(); i++)
-          Serial.print(value[i]);
-
-        Serial.println();
-        Serial.println("*********");
-      }
-    }
-};
-
+void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
+  // Callback function implementation
+  
+  switch(event) {
+    case ESP_SPP_SRV_OPEN_EVT:
+      Serial.println("Client Connected");
+      break;
+    case ESP_SPP_CLOSE_EVT:
+      Serial.println("Client diconnected");
+      break;
+  }
+}
+ 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(112500);
+  SerialBT.begin("ESP32");
+  Serial.println("ESP32");
+  SerialBT.register_callback(callback);
 
-  Serial.println("1- Download and install an BLE scanner app in your phone");
-  Serial.println("2- Scan for BLE devices in the app");
-  Serial.println("3- Connect to MyESP32");
-  Serial.println("4- Go to CUSTOM CHARACTERISTIC in CUSTOM SERVICE and write something");
-  Serial.println("5- See the magic =)");
-
-  BLEDevice::init("MyESP32");
-  BLEServer *pServer = BLEDevice::createServer();
-
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
-
-  pCharacteristic->setCallbacks(new MyCallbacks());
-
-  pCharacteristic->setValue("Hello World");
-  pService->start();
-
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
+  gyro.begin();
+  accel.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(2000);
+  char* tmp = measurement();
+  Serial.println(tmp);
+  
+  SerialBT.println(tmp);
+
+  delayMicroseconds(1);
 }
